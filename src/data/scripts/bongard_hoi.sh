750feff9df6a5305ce5b83e35c12a5acc44cef72
Adrian Kamiński <adrian22311@o2.pl>
2024-02-26 22:04:39 +0100

    add executre mode to scripts

--- src/data/scripts/bongard_hoi.sh
+++ src/data/scripts/bongard_hoi.sh
@@ -6,6 +6,7 @@ TARGET_DIR=${1:-"."}  # The directory where the data will be downloaded
 mkdir -p $TARGET_DIR/bongard_hoi
 
 URL=https://zenodo.org/record/7079175/files/bongard_hoi_images.tar
+URL_LABELS=https://zenodo.org/record/7079175/files/bongard_hoi_annotations.tar
 
 echo "--------------------------------------------------------------------------------"
 echo "Downloading BongardHOI dataset into $TARGET_DIR/bongard_hoi"
