33b7332191cef109fcb26ca8230be99f76405f84
Adrian Kamiński <adrian22311@o2.pl>
2024-03-26 19:32:40 +0100

    Refactor imports and remove commented code, add config import

--- src/main.py
+++ src/main.py
@@ -1,21 +1,15 @@
 import hydra
 import pytorch_lightning as pl
 import torch
+from hydra.core.hydra_config import HydraConfig
 from hydra.utils import instantiate
-from omegaconf import DictConfig
 from lightning.pytorch.loggers import WandbLogger
+from omegaconf import DictConfig
 
-from src.model.models.STSN import SlotAttentionAutoEncoder
-from wandb_agent import WandbAgent
-
-
-# from config import register_resolvers
+import config  # config register OmegaConf resolvers (DO NOT REMOVE IT)
 from model.avr_datasets import IRAVENdataset
-
-
-# from model.data_modules.common_modules import CombinedModuleSequential
-
-# register_resolvers()
+from model.models.STSN import SlotAttentionAutoEncoder
+from wandb_agent import WandbAgent
 
 
 @hydra.main(version_base=None, config_path="./conf", config_name="config")
@@ -36,13 +30,17 @@ def _test(cfg: DictConfig) -> None:
     trainer.logger = wandb_logger
     wandb_logger.watch(module)
     trainer.fit(module, data_module)
-    #trainer.test(module, data_module)
+    # trainer.test(module, data_module)
 
     # example loading best model from newest run
     wandb_agent = WandbAgent("AVR_universal")
     checkpoint_path = wandb_agent.get_newest_checkpoint()
     new_model = SlotAttentionAutoEncoder.load_from_checkpoint(checkpoint_path)
     print(new_model)
+
+    # The way to access ``hydra.**`` configuration
+    # print(HydraConfig.get().launcher)
+    # print(HydraConfig.get().launcher.partition)
 
 
 if __name__ == "__main__":
