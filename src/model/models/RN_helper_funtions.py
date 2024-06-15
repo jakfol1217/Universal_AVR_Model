aef481d5950f16cb12d762cd39a9bcd8524eea47
jakfol1217 <56119515+jakfol1217@users.noreply.github.com>
2024-06-16 01:41:07 +0200

    add wrens (W.I.P)

---
+++ src/model/models/RN_helper_funtions.py
@@ -0,0 +1,122 @@
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+
+
+class Identity(pl.LightningModule):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x
+
+
+class LinearBNReLU(pl.LightningModule):
+    def __init__(self, in_dim: int, out_dim: int):
+        super(LinearBNReLU, self).__init__()
+        self.linear = nn.Linear(in_dim, out_dim).to(torch.float16)
+        self.bn = nn.BatchNorm1d(out_dim)
+        self.relu = nn.ReLU()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.linear(x)
+        shape = x.shape
+        x = x.flatten(0, -2)
+        x = self.bn(x)
+        x = x.view(shape)
+        x = self.relu(x)
+        return x
+
+
+class DeepLinearBNReLU(pl.LightningModule):
+    def __init__(self, depth: int, in_dim: int, out_dim: int, change_dim_first: bool = True):
+        super(DeepLinearBNReLU, self).__init__()
+        layers = []
+        if change_dim_first:
+            layers += [LinearBNReLU(in_dim, out_dim)]
+            for _ in range(depth - 1):
+                layers += [LinearBNReLU(out_dim, out_dim)]
+        else:
+            for _ in range(depth - 1):
+                layers += [LinearBNReLU(in_dim, in_dim)]
+            layers += [LinearBNReLU(in_dim, out_dim)]
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.layers(x)
+
+
+class MLP(pl.LightningModule):
+    def __init__(self, in_dim: int, hidden_dims: List[int], out_dim: int):
+        super(MLP, self).__init__()
+        self.layers = nn.Sequential(*[
+            LinearBNReLU(d1, d2)
+            for d1, d2 in zip([in_dim] + hidden_dims, hidden_dims + [out_dim])
+        ])
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.layers(x)
+
+
+class ConvBNReLU(pl.LightningModule):
+    def __init__(self, in_channels: int, out_channels: int, **kwargs):
+        super(ConvBNReLU, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, **kwargs)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.conv(x)
+        x = self.bn(x)
+        return self.relu(x)
+
+
+class GroupPanelsIntoPairs(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, objects: torch.Tensor, objects2: torch.Tensor) -> torch.Tensor:
+        batch_size, num_images, num_panels, object_size = objects.size()
+        return torch.cat([
+            objects.unsqueeze(2).repeat(1, 1, num_panels, 1, 1),
+            objects2.unsqueeze(3).repeat(1, 1, 1, num_panels, 1)
+        ], dim=4).view(batch_size, num_images, num_panels ** 2, 2 * object_size)
+
+
+class GroupImagesIntoPairs(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, objects: torch.Tensor, objects2: torch.Tensor) -> torch.Tensor:
+        batch_size, num_images, object_size = objects.size()
+        return torch.cat([
+            objects.unsqueeze(1).repeat(1, num_images, 1, 1),
+            objects2.unsqueeze(2).repeat(1, 1, num_images, 1)
+        ], dim=3).view(batch_size, num_images ** 2, 2 * object_size)
+
+
+class GroupImagesIntoPairsWithPanels(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, objects: torch.Tensor, objects2: torch.Tensor) -> torch.Tensor:
+        batch_size, num_images, num_panels, object_size = objects.size()
+        return torch.cat([
+            objects.unsqueeze(1).repeat(1, num_images, 1, 1, 1),
+            objects2.unsqueeze(2).repeat(1, 1, num_images, 1, 1)
+        ], dim=3).view(batch_size, num_images ** 2, num_panels, 2 * object_size)
+
+
+class GroupImagesIntoPairsWith(pl.LightningModule):
+    def forward(self, objects: torch.Tensor, object: torch.Tensor) -> torch.Tensor:
+        batch_size, num_images, object_size = objects.size()
+        return torch.cat([
+            objects,
+            object.unsqueeze(1).repeat(1, num_images, 1)
+        ], dim=2)
+
+
+class GroupImagesIntoPairsWithWithPanels(pl.LightningModule):
+    def forward(self, objects: torch.Tensor, object: torch.Tensor) -> torch.Tensor:
+        batch_size, num_images, num_panels, object_size = objects.size()
+        return torch.cat([
+            objects,
+            object.unsqueeze(1).repeat(1, num_images, 1, 1)
+        ], dim=2).view(batch_size, num_images, num_panels, object_size * 2)
---
+++ src/model/models/WReN_average.py
@@ -0,0 +1,69 @@
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+
+from .RN_helper_funtions import *
+
+class WReN_average(pl.LightningModule):
+    """
+    Wild Relation Network (WReN) [1] for solving Raven's Progressive Matrices.
+    The originally proposed model uses a Relation Network (RN) [2] which works on object pairs.
+    This version compares panels that have been previously averaged.
+
+    [1] Santoro, Adam, et al. "Measuring abstract reasoning in neural networks." ICML 2018
+    [2] Santoro, Adam, et al. "A simple neural network module for relational reasoning." NeurIPS 2017
+    """
+
+    def __init__(self, object_size: int, use_layer_norm: bool = False):
+        """
+         Initializes the WReN model.
+         :param object_size: size of the object (panel) vector
+         :param use_layer_norm: flag indicating whether layer normalization should be applied after
+         the G submodule of RN.
+         """
+        super(WReN_average, self).__init__()
+
+        self.group_objects = GroupImagesIntoPairs()
+        self.group_objects_with = GroupImagesIntoPairsWith()
+
+        self.object_size = object_size
+        self.object_tuple_size = (2) * (self.object_size)
+        self.g = G(
+            depth=3,
+            in_size=self.object_tuple_size,
+            out_size=self.object_tuple_size,
+            use_layer_norm=False
+        )
+        self.norm = nn.LayerNorm(self.object_tuple_size) if use_layer_norm else Identity()
+        self.f = F(
+            depth=2,
+            object_size=self.object_tuple_size,
+            out_size=1
+        )
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, context: torch.Tensor, answers: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass of the WReN model.
+        :param x: a tensor with shape (batch_size, num_panels, height, width). num_panels is assumed
+        to be 16, as the model was designed to solve RPMs from the PGM dataset.
+        :return: a tensor with shape (batch_size, num_answers). num_answers is always equal to 8,
+        which is the number of answers for each RPM in PGM.
+        """
+        batch_context_size, num_context_images, height_context, width_context = context.size()
+        batch_answers_size, num_answer_images, height_answers, width_answers = answers.size()
+        context = context.flatten(2, -1)
+        answers = answers.flatten(2, -1)
+        pair = self.group_objects(context, context)
+        context_g_out = self.g(pair)
+        del pair
+        f_out = torch.zeros(batch_answers_size, num_answer_images, device=context.device).type_as(context)
+        for i in range(num_answer_images):
+            context_choice_pairs = self.group_objects_with(context, answers[:, i, :])
+            context_choice_g_out = self.g(context_choice_pairs)
+            relations = context_g_out + context_choice_g_out
+            relations = self.norm(relations)
+            f_out[:, i] = self.f(relations).squeeze()
+        return self.softmax(f_out)
+
+
