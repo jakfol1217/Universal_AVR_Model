d082260dde07710dc48abc83d783058005bf666b
Adrian Kamiński <adrian22311@o2.pl>
2024-07-06 20:00:30 +0200

    add functionality to train autoencoder and allow multiple scoring heads

--- src/model/models/scoring_model_v1.py
+++ src/model/models/scoring_model_v1.py
@@ -29,6 +29,7 @@ class ScoringModel(AVRModule):
         additional_metrics: dict = {},
         save_hyperparameters=True,
         freeze_slot_model=True,
+        auxiliary_loss_ratio: float = 0.0,
         **kwargs,
     ):
         super().__init__(cfg)
@@ -38,6 +39,8 @@ class ScoringModel(AVRModule):
                 OmegaConf.to_container(cfg, resolve=True, throw_on_missing=True)
             )
 
+        for _key, _value in kwargs.items():
+            self.__dict__[_key] = _value
         # self.automatic_optimization = False # use when slot and transoformer have separate optimizers
         self.in_dim = in_dim
         # self.row_column_fc = nn.Linear(6, in_dim)
@@ -50,31 +53,79 @@ class ScoringModel(AVRModule):
         else:
             self.contextnorm = False
 
-        # TODO: Add option to train slot_model as well (may require configuring multiple optimizers)
         self.slot_model = slot_model
-        if (slot_ckpt_path := cfg.model.slot_model.ckpt_path) is not None and cfg.checkpoint_path is None:
+        if (
+            slot_ckpt_path := cfg.model.slot_model.ckpt_path
+        ) is not None and cfg.checkpoint_path is None:
             cfg_dict = OmegaConf.to_container(cfg, resolve=True, throw_on_missing=True)
-            model_cfg = {k: v for k, v in cfg_dict["model"]["slot_model"].items() if k != "_target_"}
-            self.slot_model = slot_model.__class__.load_from_checkpoint(slot_ckpt_path, cfg=cfg, **model_cfg)
-
-        if freeze_slot_model:
+            model_cfg = {
+                k: v
+                for k, v in cfg_dict["model"]["slot_model"].items()
+                if k != "_target_"
+            }
+            self.slot_model = slot_model.__class__.load_from_checkpoint(
+                slot_ckpt_path, cfg=cfg, **model_cfg
+            )
+        self.freeze_slot_model = freeze_slot_model
+        self.auxiliary_loss_ratio = auxiliary_loss_ratio
+        if self.freeze_slot_model:
             self.slot_model.freeze()
+        else:
+            self.slot_model.unfreeze()
 
-        self.transformer = transformer
-        self.pos_emb = pos_emb
+        multi_transformers = [
+            int(_it.removeprefix("transformer_"))
+            for _it in kwargs.keys()
+            if _it.startswith("transformer_")
+        ]
+        multi_pos_emb = [
+            int(_it.removeprefix("pos_emb_"))
+            for _it in kwargs.keys()
+            if _it.startswith("pos_emb_")
+        ]
+        assert len(multi_transformers) == len(
+            multi_pos_emb
+        ), "Number of transformers and positional embeddings should be equal"
+
+        if len(multi_transformers) == 0:
+            self.transformer = nn.ModuleList([transformer])
+            self.pos_emb = nn.ModuleList([pos_emb])
+        if len(multi_transformers) > 0:
+            self.transformer = nn.ModuleList(
+                [transformer]
+                + [kwargs.get(f"transformer_{_ix}") for _ix in multi_transformers]
+            )
+            self.pos_emb = nn.ModuleList(
+                [pos_emb] + [kwargs.get(f"pos_emb_{_ix}") for _ix in multi_pos_emb]
+            )
 
         self.loss = instantiate(cfg.metrics.cross_entropy)
         self.val_losses = []
-        self.additional_metrics = nn.ModuleDict(
-            {
-                metric_nm: (
-                    instantiate(metric_func)
-                    if isinstance(metric_func, DictConfig)
-                    else metric_func
-                )
-                for metric_nm, metric_func in additional_metrics.items()
-            }
-        )
+
+        def create_module_dict(metrics_dict):
+            return nn.ModuleDict(
+                {
+                    metric_nm: (
+                        instantiate(metric_func)
+                        if isinstance(metric_func, DictConfig)
+                        else metric_func
+                    )
+                    for metric_nm, metric_func in metrics_dict.items()
+                }
+            )
+
+        if len(multi_transformers) == 0:
+            self.additional_metrics = nn.ModuleList(
+                [create_module_dict(additional_metrics)]
+            )
+        else:
+            self.additional_metrics = nn.ModuleList(
+                [create_module_dict(additional_metrics)]
+                + [
+                    create_module_dict(kwargs.get(f"additional_metrics_{_ix}"))
+                    for _ix in multi_transformers
+                ]
+            )
 
     def apply_context_norm(self, z_seq):
         eps = 1e-8
@@ -87,17 +138,19 @@ class ScoringModel(AVRModule):
         ).unsqueeze(0)
         return z_seq
 
-    def forward(self, given_panels, answer_panels):
-        # TODO: implement with slot_model (image as input instead of slots)
+    def forward(self, given_panels, answer_panels, idx=0):
+        __pos_emb = self.pos_emb[idx]
+        __transformer = self.transformer[idx]
+        __num_correct = (
+            self.num_correct if idx == 0 else self.__dict__[f"num_correct_{idx}"]
+        )
 
         scores = []
         pos_emb_score = (
-            self.pos_emb(given_panels)
-            if self.pos_emb is not None
-            else torch.tensor(0.0)
+            __pos_emb(given_panels) if __pos_emb is not None else torch.tensor(0.0)
         )
         # Loop through all choices and compute scores
-        for d in permutations(range(answer_panels.shape[1]), self.num_correct):
+        for d in permutations(range(answer_panels.shape[1]), __num_correct):
 
             # print(AB,C_choices[:,d,:],AB.shape,C_choices[:,d,:].shape)
             # x_seq = torch.cat([given_panels_posencoded_seq,torch.cat((answer_panels[:,d],self.row_fc(third).unsqueeze(1).repeat((1,answer_panels.shape[2],1)), self.column_fc(third).unsqueeze(1).repeat((1,answer_panels.shape[2],1))),dim=2).unsqueeze(1)],dim=1)
@@ -111,9 +164,9 @@ class ScoringModel(AVRModule):
 
                 x_seq = self.apply_context_norm(x_seq)
 
-            x_seq = x_seq + pos_emb_score  # TODO: add positional embeddings
+            x_seq = x_seq + pos_emb_score
             # x_seq = torch.cat((x_seq,all_posemb_concat_flatten),dim=2)
-            score = self.transformer(x_seq)
+            score = __transformer(x_seq)
             scores.append(score)
         scores = torch.cat(scores, dim=1)
         return scores
@@ -147,20 +200,39 @@ class ScoringModel(AVRModule):
         given_panels = torch.stack(slots_seq, dim=1)[:, :context_panels_cnt]
         answer_panels = torch.stack(slots_seq, dim=1)[:, context_panels_cnt:]
 
-        scores = self(given_panels, answer_panels)
+        scores = self(given_panels, answer_panels, idx=dataloader_idx)
         # print("scores and target>>",scores,target)
         pred = scores.argmax(1)
         # print(scores)
         # print(scores.shape) # batch x num_choices
         # print(f"Prediction: {pred}, Target: {target}")
+        ce_loss = self.loss(scores, target)
 
-        for metric_nm, metric_func in self.additional_metrics.items():
+        current_metrics = self.additional_metrics[dataloader_idx]
+        for metric_nm, metric_func in current_metrics.items():
             value = metric_func(pred, target)
             self.log(
                 f"{step_name}/{self.task_names[dataloader_idx]}/{metric_nm}",
                 value,
                 on_epoch=True,
-                prog_bar=True if step_name == "val" else False,
+                prog_bar=False,
+                logger=True,
+                add_dataloader_idx=False,
+            )
+        if self.auxiliary_loss_ratio > 0:
+            self.log(
+                f"{step_name}/{self.task_names[dataloader_idx]}/mse_loss",
+                slot_model_loss,
+                on_epoch=True,
+                prog_bar=False,
+                logger=True,
+                add_dataloader_idx=False,
+            )
+            self.log(
+                f"{step_name}/{self.task_names[dataloader_idx]}/cross_entropy_loss",
+                ce_loss,
+                on_epoch=True,
+                prog_bar=False,
                 logger=True,
                 add_dataloader_idx=False,
             )
@@ -171,8 +243,7 @@ class ScoringModel(AVRModule):
         # print("recon combined seq shape>>",torch.stack(recon_combined_seq,dim=1).shape)
         # loss = 1000*mse_criterion(torch.stack(recon_combined_seq,dim=1), img) + ce_criterion(scores,target)
         # loss = ce_criterion(scores,target)
-        # TODO: which loss
-        loss = self.loss(scores, target)
+        loss = ce_loss + self.auxiliary_loss_ratio * slot_model_loss
         return loss
 
     def training_step(self, batch, batch_idx, dataloader_idx=0):
