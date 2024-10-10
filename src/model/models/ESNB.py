bc1f1a8b512257ad11dc3f315bb9e589bb41cbc2
Adrian Kamiński <akaminski@eden>
2024-10-10 22:37:37 +0200

    add ESNB relation module and E2E model with it

---
+++ src/model/models/ESNB.py
@@ -0,0 +1,201 @@
+import numpy as np
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from omegaconf import DictConfig, OmegaConf
+
+
+class ESNB(pl.LightningModule):
+    def __init__(
+        self,
+        cfg: DictConfig,
+        # y_dim,  # TODO: remove?
+        # encoder_model,
+        norm_type: str,  # "contextnorm" | "tasksegmented_contextnorm" | None
+        task_seg: list[list[int]] | None,  # Task segmentation (for tasksegmented_contextnorm - pass task specific information)
+        z_size: int,  # = 128 - embedding size (if lower the value will be padded with 0)
+        key_size: int,  # = 256 - LSTM input_size
+        hidden_size: int,  # = 512 - LSTM hidden size
+        save_hyperparameters=False,
+        **kwargs,
+    ):
+        super().__init__()
+
+        if save_hyperparameters:
+            self.save_hyperparameters(
+                OmegaConf.to_container(cfg, resolve=True, throw_on_missing=True)
+            )
+
+        # # Encoder
+        # if args.encoder == 'conv':
+        # 	self.encoder = Encoder_conv(args)
+        # elif args.encoder == 'mlp':
+        # 	self.encoder = Encoder_mlp(args)
+        # elif args.encoder == 'rand':
+        # 	self.encoder = Encoder_rand(args)
+        # LSTM and output layers
+
+        self.z_size = z_size
+        self.key_size = key_size
+        self.hidden_size = hidden_size
+        self.lstm = nn.LSTM(self.key_size + 1, self.hidden_size, batch_first=True)
+        self.key_w_out = nn.Linear(self.hidden_size, self.key_size)
+        self.g_out = nn.Linear(self.hidden_size, 1)
+        self.confidence_gain = nn.Parameter(torch.ones(1))
+        self.confidence_bias = nn.Parameter(torch.zeros(1))
+        # self.y_out = nn.Linear(self.hidden_size, y_dim)  # TODO: remove?
+        # Context normalization
+        self.contextnorm = False
+        if norm_type == "contextnorm" or norm_type == "tasksegmented_contextnorm":
+            self.contextnorm = True
+            self.gamma = nn.Parameter(torch.ones(self.z_size))
+            self.beta = nn.Parameter(torch.zeros(self.z_size))
+
+        self.task_seg = None
+        if norm_type == "tasksegmented_contextnorm":
+            self.task_seg = task_seg
+        # else:
+        # 	self.task_seg = [np.arange(task_gen.seq_len)]
+        # Nonlinearities
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+        self.softmax = nn.Softmax(dim=1)
+        # Initialize parameters
+        for name, param in self.named_parameters():
+            # Encoder parameters have already been initialized
+            if "encoder" not in name and "confidence" not in name:
+                # Initialize all biases to 0
+                if "bias" in name:
+                    nn.init.constant_(param, 0.0)
+                else:
+                    if "lstm" in name:
+                        # Initialize gate weights (followed by sigmoid) using Xavier normal distribution
+                        nn.init.xavier_normal_(param[: self.hidden_size * 2, :])
+                        nn.init.xavier_normal_(param[self.hidden_size * 3 :, :])
+                        # Initialize input->hidden and hidden->hidden weights (followed by tanh) using Xavier normal distribution with gain =
+                        nn.init.xavier_normal_(
+                            param[self.hidden_size * 2 : self.hidden_size * 3, :],
+                            gain=5.0 / 3.0,
+                        )
+                    elif "key_w" in name:
+                        # Initialize weights for key output layer (followed by ReLU) using Kaiming normal distribution
+                        nn.init.kaiming_normal_(param, nonlinearity="relu")
+                    elif "g_out" in name:
+                        # Initialize weights for gate output layer (followed by sigmoid) using Xavier normal distribution
+                        nn.init.xavier_normal_(param)
+                    # elif "y_out" in name:  # TODO: remove?
+                    #     # Initialize weights for multiple-choice output layer (followed by softmax) using Xavier normal distribution
+                    #     nn.init.xavier_normal_(param)
+
+    def forward(self, z_seq):
+        z_seq = F.pad(
+            z_seq, (0, self.z_size - z_seq.shape[-1]), mode="constant", value=0
+        )
+        # print("##", z_seq.isnan().sum())
+        if self.contextnorm:
+            z_seq_all_seg = []
+            task_seg = self.task_seg if self.task_seg else [np.arange(z_seq.shape[1])]
+            for seg in range(len(task_seg)):
+                z_seq_all_seg.append(
+                    self.apply_context_norm(z_seq[:, task_seg[seg], :])
+                )
+            z_seq = torch.cat(z_seq_all_seg, dim=1)
+        # print("#", z_seq.isnan().sum())
+        z_seq = torch.nan_to_num(z_seq, nan=0.0)
+        # process slots one by one
+        if z_seq.dim() == 4:
+            z_seq = z_seq.view(
+                z_seq.shape[0], z_seq.shape[1] * z_seq.shape[2], z_seq.shape[3]
+            )
+        # print("#", z_seq.isnan().sum())
+
+        # Initialize hidden state
+        hidden = torch.zeros(1, z_seq.shape[0], self.hidden_size, device=z_seq.device)  # x_seq.shape
+        cell_state = torch.zeros(1, z_seq.shape[0], self.hidden_size, device=z_seq.device)  # x_seq.shape
+        # Initialize retrieved key vector
+        key_r = torch.zeros(z_seq.shape[0], 1, self.key_size + 1, device=z_seq.device)  # x_seq.shape
+        # Memory model
+        # all_key_r = []
+        # print(f"{z_seq.shape=}")
+        for t in range(z_seq.shape[1]):  # x_seq.shape
+            # print("*", t)
+            # Image embedding
+            z_t = torch.clamp(z_seq[:, t, :].unsqueeze(1), min=-1e6, max=1e6)
+            # print(f"{z_t.isnan().sum()=}")
+            # print(f"{z_t.shape=}")
+            # Controller
+            # LSTM
+            # print(key_r.device, hidden.device, cell_state.device)
+            lstm_out, (hidden, cell_state) = self.lstm(key_r, (hidden, cell_state))
+            # Key output layers
+            key_w = self.relu(self.key_w_out(lstm_out))
+            # Gates
+            g = self.sigmoid(self.g_out(lstm_out))
+            # Task output layer
+            # y_pred_linear = self.y_out(lstm_out).squeeze()
+            # y_pred = y_pred_linear.argmax(1)
+            # Read from memory
+            # print(f"{key_w.isnan().sum()=}")
+            # print(f"{g.isnan().sum()=}")
+            # print(f"{lstm_out.isnan().sum()=}")
+            
+            if t == 0:
+                key_r = torch.zeros(z_seq.shape[0], 1, self.key_size + 1, device=z_seq.device)  # x_seq
+            else:
+                # Read key
+
+                # not necessary if slots are passed to lstm one by one
+                # if z_t.dim() == 4:
+                #     z_t = z_t.mean(2)
+                w_k = self.softmax((z_t * M_v).sum(dim=2))
+                c_k = self.sigmoid(
+                    ((z_t * M_v).sum(dim=2) * self.confidence_gain)
+                    + self.confidence_bias
+                )
+                key_r = g * (
+                    torch.cat([M_k, c_k.unsqueeze(2)], dim=2) * w_k.unsqueeze(2)
+                ).sum(1).unsqueeze(1)
+                
+                # print(f"{M_v.isnan().sum()=}")
+                # print(f"{z_t.isnan().sum()=}")
+                # print(f"{M_v.max()=}")
+                # print(f"{z_t.max()=}")
+                # print(f"{w_k.isnan().sum()=}")
+                # print(f"{c_k.isnan().sum()=}")
+                # if w_k.isnan().sum()!=0:
+                #     print(f"{z_t=}")
+                #     print(f"{w_k=}")
+                #     print(f"{M_v=}")
+                #     print(f"{(z_t * M_v).sum(dim=2).isnan().sum()=}")
+                #     print(f"{self.softmax((z_t * M_v).sum(dim=2)).isnan().sum()=}")
+                #     raise Exception("Wops")
+                
+                # all_key_r.append(key_r)
+            # Write to memory
+            if t == 0:
+                M_k = key_w
+                # not necessary if slots are passed to lstm one by one
+                # if z_t.dim() == 4:
+                #     M_v = z_t.mean(2)
+
+                M_v = z_t
+            else:
+                M_k = torch.cat([M_k, key_w], dim=1)
+                M_v = torch.cat([M_v, z_t], dim=1)
+
+        # Memory model (extra time step to process key retrieved on final time step)
+        z_t = torch.zeros(z_seq.shape[0], 1, self.z_size, device=z_seq.device)  # x_seq.shape
+        # print(f"{z_t=}")
+        # print(f"{lstm_out=}")
+        # LSTM
+        lstm_out, (hidden, cell_state) = self.lstm(key_r, (hidden, cell_state))
+        return lstm_out.squeeze(1)
+
+    def apply_context_norm(self, z_seq):
+        eps = 1e-8
+        z_mu = z_seq.mean(1)
+        z_sigma = (z_seq.var(1) + eps).sqrt()
+        z_seq = (z_seq - z_mu.unsqueeze(1)) / z_sigma.unsqueeze(1)
+        z_seq = (z_seq * self.gamma) + self.beta
+        return z_seq
---
+++ src/model/models/scoring_model_esnb.py
@@ -0,0 +1,237 @@
+import io
+import math
+import os
+from itertools import permutations
+from typing import OrderedDict
+
+import h5py
+import numpy as np
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from hydra.utils import get_class, instantiate
+from omegaconf import DictConfig, OmegaConf
+from torch import nn
+from torch.utils.checkpoint import checkpoint
+
+from model.models import STSN, STSNv3
+
+from .base import AVRModule
+# import sys
+
+# print(f">>>pl::{pl.__version__}")
+# print(f">>>torch::{torch.__version__}")
+# sys.setrecursionlimit(50000)
+
+class ScoringModelEsnb(AVRModule):
+    def __init__(
+        self,
+        cfg: DictConfig,
+        encoders: list[pl.LightningDataModule],
+        relation_module: pl.LightningModule,
+        scoring_module: pl.LightningModule,
+        save_hyperparameters=True,
+        auxiliary_loss_ratio: float = 0.0,
+        increment_dataloader_idx: int = 0,
+        **kwargs,
+    ):
+        super().__init__(cfg)
+
+        if save_hyperparameters:
+            self.save_hyperparameters(
+                OmegaConf.to_container(cfg, resolve=True, throw_on_missing=True)
+            )
+
+        _encoders = []
+        cfg_dict = OmegaConf.to_container(cfg, resolve=True, throw_on_missing=True)
+        for _i in range(len(encoders)):
+            conf = cfg.model.encoders[_i]
+            if conf is None:
+                module=None
+            elif (
+                ckpt_path := conf.ckpt_path
+            ) is not None and cfg.checkpoint_path is None:
+                _cfg_dict = cfg_dict["model"]["encoders"][_i]
+                model_cfg = {
+                    k: v
+                    for k, v in _cfg_dict.items()
+                    if k != "_target_"
+                }
+
+                module_class = get_class(conf._target_)
+                module = module_class.load_from_checkpoint(
+                    ckpt_path, cfg=cfg, **model_cfg
+                )
+            else:
+                if isinstance(encoders[_i], DictConfig):
+                    _args = [cfg] if encoders[_i].get("_target_") != "timm.create_model" else []
+                    module=instantiate(encoders[_i], *_args)
+                else:
+                    module=encoders[_i]
+
+            if conf and conf.get('freeze', True):
+                for param in module.parameters():
+                    param.requires_grad = False
+            _encoders.append(module)
+
+        self.encoders = nn.ModuleList(_encoders)
+        self.relation_module =  relation_module
+        self.scoring_module = scoring_module
+
+        self.auxiliary_loss_ratio = auxiliary_loss_ratio
+
+        self.loss = instantiate(cfg.metrics.cross_entropy)
+        self.val_losses = []
+        self.increment_dataloader_idx = increment_dataloader_idx
+
+        def create_module_dict(metrics_dict):
+            return nn.ModuleDict(
+                {
+                    metric_nm: (
+                        instantiate(metric_func)
+                        if isinstance(metric_func, DictConfig)
+                        else metric_func
+                    )
+                    for metric_nm, metric_func in metrics_dict.items()
+                }
+            )
+
+
+        task_metrics_idxs = sorted([
+            int(_it.removeprefix("task_metric_"))
+            for _it in kwargs.keys()
+            if _it.startswith("task_metric_")
+        ])
+        self.task_metrics = nn.ModuleList(
+            [
+                create_module_dict(kwargs.get(f"task_metric_{_ix}"))
+                for _ix in task_metrics_idxs
+            ]
+        )
+
+        if len(self.task_metrics) > 0:
+            self.additional_metrics = self.task_metrics
+
+
+    def apply_context_norm(self, z_seq):
+        eps = 1e-8
+        z_mu = z_seq.mean(1)
+        z_sigma = (z_seq.var(1) + eps).sqrt()
+        z_seq = (z_seq - z_mu.unsqueeze(1)) / z_sigma.unsqueeze(1)
+        z_seq = (z_seq * self.gamma.unsqueeze(0).unsqueeze(0)) + self.beta.unsqueeze(
+            0
+        ).unsqueeze(0)
+        return z_seq
+
+    def forward(self, relations, idx=0):
+        return self.scoring_module(relations)
+
+    # TODO: Separate optimizers for different modules
+    def configure_optimizers(self):
+        return instantiate(self.cfg.optimizer, params=self.parameters())
+
+    def _step(self, step_name, batch, batch_idx, dataloader_idx=0):
+        img, target = batch
+        encoder = self.encoders[dataloader_idx + self.increment_dataloader_idx]
+        results = []
+
+        if encoder is None:
+            encoder_model_loss = 0.0
+            panels = img
+        elif isinstance(encoder, (STSN.SlotAttentionAutoEncoder, STSNv3.SlotAttentionAutoEncoder)):
+            recon_combined_seq = []
+            recons_seq = []
+            masks_seq = []
+            for idx in range(img.shape[1]):
+                recon_combined, recons, masks, slots, attn = encoder(img[:, idx])
+                recons_seq.append(recons)
+                recon_combined_seq.append(recon_combined)
+                masks_seq.append(masks)
+                results.append(slots)
+                del recon_combined, recons, masks, slots, attn
+            pred_img = torch.stack(recon_combined_seq, dim=1).contiguous()
+            if pred_img.shape[2] != img.shape[2]:
+                pred_img = pred_img.repeat(1, 1, 3, 1, 1)
+            encoder_model_loss = encoder.loss(pred_img, img)
+            panels = torch.stack(results, dim=1)
+        else:
+            for idx in range(img.shape[1]):
+            #     with torch.autocast(device_type='cuda', dtype=torch.float16):
+                res = encoder(img[:, idx])
+                results.append(res)
+            encoder_model_loss = 0.0
+            panels = torch.stack(results, dim=1)
+
+        
+
+        relations = self.relation_module(panels)
+        # print(f"{relations=}")
+        scores = self(relations)
+
+        pred = scores.argmax(1)
+        # print(scores)
+        # print(scores.shape) # batch x num_choices
+        # print(f"Prediction: {pred}, Target: {target}")
+        ce_loss = self.loss(scores, target)
+
+        current_metrics = self.additional_metrics[dataloader_idx + self.increment_dataloader_idx]
+        for metric_nm, metric_func in current_metrics.items():
+            value = metric_func(pred, target)
+            self.log(
+                f"{step_name}/{self.task_names[dataloader_idx]}/{metric_nm}",
+                value,
+                on_epoch=True,
+                prog_bar=False,
+                logger=True,
+                add_dataloader_idx=False,
+            )
+        if self.auxiliary_loss_ratio > 0:
+            self.log(
+                f"{step_name}/{self.task_names[dataloader_idx]}/mse_loss",
+                encoder_model_loss,
+                on_epoch=True,
+                prog_bar=False,
+                logger=True,
+                add_dataloader_idx=False,
+            )
+            self.log(
+                f"{step_name}/{self.task_names[dataloader_idx]}/cross_entropy_loss",
+                ce_loss,
+                on_epoch=True,
+                prog_bar=False,
+                logger=True,
+                add_dataloader_idx=False,
+            )
+
+        loss = ce_loss + self.auxiliary_loss_ratio * encoder_model_loss
+        # print(f"{encoder_model_loss=}")
+        # print(f"{loss=}")
+        return loss
+
+    def training_step(self, batch, batch_idx, dataloader_idx=0):
+        loss = self.module_step("train", batch, batch_idx, dataloader_idx)
+        return loss
+
+    def validation_step(self, batch, batch_idx, dataloader_idx=0):
+        loss = self.module_step("val", batch, batch_idx, dataloader_idx)
+        self.val_losses.append(loss)
+        return loss
+
+    def test_step(self, batch, batch_idx, dataloader_idx=0):
+        loss = self.module_step("test", batch, batch_idx, dataloader_idx)
+        return loss
+
+    def on_validation_epoch_end(self) -> None:
+        val_losses = torch.tensor(self.val_losses)
+        val_loss = val_losses.nanmean()
+        self.log(
+            "val/loss",
+            val_loss.to(self.device),
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+            add_dataloader_idx=False,
+            sync_dist=True,
+        )
+        self.val_losses.clear()
