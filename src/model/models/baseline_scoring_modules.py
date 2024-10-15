4b19f7f75f6a51734ffdb6c7deeee6bed82c9bb6
jakfol1217 <56119515+jakfol1217@users.noreply.github.com>
2024-10-16 00:24:32 +0200

    add baseline models

---
+++ src/model/models/baseline_scoring_modules.py
@@ -0,0 +1,68 @@
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+from omegaconf import DictConfig
+
+from .scoring_model_feature_transformer_v1 import ScoringModelFeatureTransformer
+
+class BaselineScoringModel(ScoringModelFeatureTransformer):
+    def __init__(self, 
+                cfg: DictConfig,
+                context_norm: bool,
+                 base_type: str, 
+                 base_in_dim: int, 
+                 base_out_dim: int, 
+                 base_hid_dim: int, 
+                 *args, 
+                 **kwargs):
+        super().__init__(cfg=cfg, context_norm=context_norm, *args, **kwargs)
+
+        self.base_type = base_type
+        if base_type == "mlp":
+            self.scoring = MLPscoringModule(base_in_dim, base_out_dim, base_hid_dim)
+        if base_type == "avg_pool":
+            self.scoring = PoolingScoringModule(base_out_dim, "avg")
+        if base_type == "max_pool":
+            self.scoring = PoolingScoringModule(base_out_dim, "max")
+
+    def forward(self, given_panels, answer_panels, idx=0):
+
+        x_seq = torch.cat([given_panels, answer_panels], dim=1)
+
+        if self.contextnorm:
+            x_seq = self.apply_context_norm(x_seq)
+
+        if self.base_type == "mlp":     
+            x_seq = torch.flatten(x_seq, start_dim=1, end_dim=2)
+
+        scores = self.scoring(x_seq)
+
+        return scores
+
+
+class MLPscoringModule(pl.LightningModule):
+    def __init__(self, in_dim: int, out_dim: int, hidden_dim: int = None):
+        super().__init__()
+        if hidden_dim is not None:
+            self.mlp_module = nn.Sequential(
+                nn.Linear(in_dim, hidden_dim),
+                nn.Linear(hidden_dim, out_dim)
+            )
+        else:
+            self.mlp_module = nn.Sequential(
+                nn.Linear(in_dim, out_dim)
+            )
+    
+    def forward(self, panels):
+        return self.mlp_module(panels)
+
+class PoolingScoringModule(pl.LightningModule):
+    def __init__(self, out_dim: int, pool_type: str = "max"):
+        super().__init__()
+        if pool_type == "max":
+            self.pooling = nn.AdaptiveMaxPool2d((1, out_dim))
+        elif pool_type == "avg":
+            self.pooling = nn.AdaptiveAvgPool2d((1, out_dim))
+
+    def forward(self, panels):
+        return self.pooling(panels).squeeze(1)
